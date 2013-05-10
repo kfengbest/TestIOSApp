8bbf6ef4c2abb5d86108c384e4bcc92159f629ad
kfengbest <kfengbest@hotmail.com>
2013-05-10 22:25:50 +0800

    init empty project

---
+++ TestIOSApp/TestIOSApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestIOSApp
+//
+//  Created by Kaven Feng on 5/10/13.
+//  Copyright (c) 2013 Kaven Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
