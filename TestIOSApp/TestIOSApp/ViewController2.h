2e4ed3f2c422af2f5a752f4312ee59d6bf0ccbfb
kfengbest <kfengbest@hotmail.com>
2013-05-10 23:02:37 +0800

    programally create UINavigatoinBar and push|pop UINavigationItem

---
+++ TestIOSApp/TestIOSApp/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  TestEmptyApp
+//
+//  Created by Kaven Feng on 5/10/13.
+//  Copyright (c) 2013 Kaven Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
---
+++ TestIOSApp/TestIOSApp/ViewController2.m
@@ -0,0 +1,40 @@
+//
+//  ViewController2.m
+//  TestEmptyApp
+//
+//  Created by Kaven Feng on 5/10/13.
+//  Copyright (c) 2013 Kaven Feng. All rights reserved.
+//
+
+#import "ViewController2.h"
+
+@interface ViewController2 ()
+
+@end
+
+@implementation ViewController2
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        
+        
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
