9cefaf341cf810090ba3d8044120053ff57c8c2b
kfengbest <kfengbest@hotmail.com>
2013-05-10 23:44:39 +0800

    Dynamic create and push view controller using navigation controller

--- TestIOSApp/TestIOSApp/ViewController1.m
+++ TestIOSApp/TestIOSApp/ViewController1.m
@@ -9,7 +9,9 @@
 #import "ViewController1.h"
 
 @interface ViewController1 ()
-
+{
+    int nClicked;
+}
 @end
 
 @implementation ViewController1
@@ -19,6 +21,8 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        
+        nClicked = 0;
         
         self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:[NSString stringWithFormat:@"Create%d",1] style:UIBarButtonItemStylePlain target:self action:@selector(onCreate1:)];
         
@@ -41,8 +45,18 @@
 
 - (void) onCreate1 : (id)sender{
     
+    nClicked++;
+    
     UIViewController* vc = [[UIViewController alloc] init];
     vc.view.backgroundColor = [UIColor yellowColor];
+    vc.title = [NSString stringWithFormat:@"VC%d",nClicked];
+    
+    UIButton* btn2 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [vc.view addSubview:btn2];
+    [btn2 setTitle:[NSString stringWithFormat:@"VC%d",nClicked] forState:UIControlStateNormal];
+    btn2.frame = CGRectMake(20, 100, 120, 44);
+    [btn2 addTarget:self action:@selector(onCreate1:) forControlEvents:UIControlEventTouchUpInside];
+
     
     [self.navigationController pushViewController:vc animated:YES];
     
